4ca3a3df900050c1842c7cbe6a7159818e3e4798
Kahiro-M <0204.kahiro@gmail.com>
2025-09-17 16:48:47 +0900

    ファイルのダウンロド可否を制御するフィールドの設定を追加

--- download.php
+++ download.php
@@ -20,7 +20,7 @@ if (!preg_match(
     '/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i',
     $uuid
 )) {
-    die("Invalid ffindPath.");
+    die("ファイルが見つかりません。ファイル識別子を確認してください。Err0001");
 }
 
 // 該当する全ファイルを取得
@@ -28,7 +28,7 @@ $findPath = $baseDir . '/' . $uuid . '.*';
 $files = glob($findPath);
 
 if (empty($files)) {
-    die("ファイルが見つかりません。ファイル識別子を確認してください。");
+    die("ファイルが見つかりません。ファイル識別子を確認してください。Err0002");
 }
 
 // ========== ダウンロード実行モード ==========
@@ -36,6 +36,14 @@ if (isset($_GET['ext'])) {
     // kintoneから該当レコード情報取得
     $record = getKintoneRecord(KINTONE_API_TOKEN, KINTONE_APP_ID, FILE_UUID_FIELD_CODE, $uuid);
     $record = $record['records'][0] ?? null;
+
+    // 死活チェック
+    foreach (IS_DISABLE_FILED_CODE as $fieldCode => $disableValues) {
+        $fieldValue = $record[$fieldCode]['value'] ?? '';
+        if (in_array($fieldValue, $disableValues, true)) {
+            die("ファイルが見つかりません。ファイルを確認してください。Err0004");
+        }
+    }
 
     // ダウンロードファイル名生成
     $fileTitle = getDownloadFileName($record);
@@ -45,7 +53,7 @@ if (isset($_GET['ext'])) {
     $targetFile = $baseDir . '/' . $uuid . '.' . $ext;
 
     if (!file_exists($targetFile)) {
-        die("ファイルが見つかりません。ファイル識別子、拡張子を確認してください。");
+        die("ファイルが見つかりません。ファイル識別子、拡張子を確認してください。Err0003");
     }
 
     // MIMEタイプ判定
